737b4f389e713df566940f23db0de404cb745c8e
Wildlifes <2632003786@qq.com>
2019-12-31 09:29:56 +0800

    :sparkles: add new branch gallery

--- lib/views/login.dart
+++ lib/views/login.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_template/route/application.dart';
+import 'package:flutter_template/route/routes.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -14,12 +15,8 @@ class _LoginPageState extends State<LoginPage> {
         child: Text("12"),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          SharedPreferences prefs = await SharedPreferences.getInstance();
-          int counter = (prefs.getInt('counter') ?? 0) + 1;
-          print('Pressed $counter times.');
-          await prefs.setInt('counter', counter);
-//          Application.navigateTo(context, Routes.i18n, {});
+        onPressed: () {
+          Application.navigateTo(context, Routes.i18n, {});
         },
         child: Text("12"),
       ),
